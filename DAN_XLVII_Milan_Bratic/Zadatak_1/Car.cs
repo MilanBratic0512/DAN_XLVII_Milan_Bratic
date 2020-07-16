2f6a300298ac33b85e378c7962480dcb69ee1565
MilanBratic0512 <milan.bratic2@gmail.com>
2020-07-16 15:02:29 +0200

    create bridge, car and direction class

---
+++ DAN_XLVII_Milan_Bratic/Zadatak_1/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak_1
+{
+    class Car
+    {
+        public string name { get; set; }
+        Bridge bridge;
+
+        public Car(Bridge bridge)
+        {
+            this.bridge = bridge;
+        }
+        
+        public void Run()
+        {
+            bridge.OneVehicle(this);
+        }
+    }
+}
--- DAN_XLVII_Milan_Bratic/Zadatak_1/Program.cs
+++ DAN_XLVII_Milan_Bratic/Zadatak_1/Program.cs
@@ -2,14 +2,53 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Zadatak_1
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            Bridge bridge = new Bridge();
+
+            int max = 15;
+
+            int countS = 0;
+            Thread thSouth = new Thread(() =>
+            {
+
+                while (countS++ < max)
+                {
+                    Car car = new Car(bridge);
+
+                    Thread th = new Thread(car.Run);
+                    car.name = "South Car:" + th.ManagedThreadId;
+                    th.Start();
+                    Thread.Sleep(500);
+                }
+
+            });
+            int countN = 0;
+            Thread thNorth = new Thread(() =>
+            {
+
+                while (countN++ < max)
+                {
+                    Car car = new Car(bridge);
+
+                    Thread th = new Thread(car.Run);
+                    car.name = "North Car:" + th.ManagedThreadId;
+                    th.Start();
+                    Thread.Sleep(500);
+                }
+
+            });
+            thSouth.Start();
+            thNorth.Start();
+            Console.ReadLine();
         }
     }
 }
